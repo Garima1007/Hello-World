b876e1fb2f68c73fd7fe30a5305bb7280ab15c3f
Garima1007 <sharma.garima1007@gmail.com>
2023-05-19 17:54:12 +0700

    added more test

--- hello/hello1.java
+++ hello/hello1.java
@@ -6,6 +6,7 @@ public class hello1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World...");
 		System.out.println("This is Jenkins Integration with Git");
+		System.out.println("One more automated deployment");
 
 	}
 
